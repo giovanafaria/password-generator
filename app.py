1657df85e8e2684a92119c48b739e2d46d106daa
Giovana Faria <giovanafaria@Giovanas-MacBook-Air-2.local>
2024-02-12 15:10:45 -0300

    Updating app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import secrets
 import string
-import random
 import pyperclip
 import os
 import time
@@ -11,31 +10,59 @@ characters = {
     'symbols': string.punctuation,
 }
 
+
 def enter_password_length():
-    try:
-        n = int(input('\tEnter the length of your password: '))
-        return n  # return the length of the password
-    except ValueError:
-        print('\tInvalid input. Value should be integer only.')
-        print('Restarting program...')
-        time.sleep(3)
-        main()  # restart the program if input is invalid
+    """
+    This function prompts the user to enter the length of the password they would like to generate.
+    It returns the length of the password as an integer.
+
+    Args:
+        None
+
+    Returns:
+        int: The length of the password as an integer
+
+    Raises:
+        ValueError: If the input is not a valid integer
+    """
+    while True:
+        try:
+            n = int(input('\tEnter the length of your password: '))
+            return n  # return the length of the password
+        except ValueError:
+            print('\tInvalid input. Value should be integer only.')
+            print('Restarting program...')
+            time.sleep(3)
 
 
 def generate_password(n: int) -> str:
+    """ 
+    Generate a random password of length n.
+
+    Args:
+        n (int): The length of the password to generate.
+
+    Returns:
+        str: The generated password.
+    """ 
     password = ''
-    for i in range(n):
-        password += ''.join(secrets.choice(characters['letters'] + characters['numbers'] + characters['symbols']))
+    for i in range(n):  # generates the password randomly with the range specified
+        password += ''.join(secrets.choice(characters['letters'] +
+                                           characters['numbers'] +
+                                           characters['symbols']))
     return password
 
 
 def main():
     os.system('clear')  # clear the terminal
     print('Startig password generator...')
-    password_length = enter_password_length()
+    while True: 
+        password_length = enter_password_length()
+        if password_length > 0:  # check if input if valid
+            break
     password = generate_password(password_length)
     print('Generated password: ' + password)
-    pyperclip.copy(password)
+    pyperclip.copy(password)  # coping password to clipboard
     print('Password copied to clipboard.')
 
 
